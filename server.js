e4653da7bf2b63f0245c7f6556b9f9a6e8da11f5
Moorthi15 <moorthi15072000@gmail.com>
2025-11-24 19:44:33 +0530

    Remove user routes from server.js
    Removed user routes middleware from server setup.

--- server.js
+++ server.js
@@ -3,19 +3,17 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 
-import userRoutes from "./routes/users.js";
 import taskRoutes from "./routes/tasks.js";
 import withdrawRoutes from "./routes/withdraw.js";
 import adminRoutes from "./routes/admin.js";
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
-app.use("/user", userRoutes);
+// ❌ remove this → app.use("/user", userRoutes);
 app.use("/tasks", taskRoutes);
 app.use("/withdraw", withdrawRoutes);
 app.use("/admin", adminRoutes);
@@ -25,7 +23,6 @@ app.get("/", (req, res) => {
     res.send("Telegram Mini App Backend Running ✔️");
 });
 
-// Start server
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
